6b7fc6efe572e60a6010a6d1055070c80daad522
jpbn456 <jpbortol@gmail.com>
2023-06-08 16:55:38 -0300

    New Height Check Implementation added

--- Ejercicio6-Bortol-Fungo.py
+++ Ejercicio6-Bortol-Fungo.py
@@ -2,55 +2,67 @@ from decimal import Decimal, getcontext
 import matplotlib.pyplot as plt
 import pandas as pd
 
-
 getcontext().prec = 7
 
-STEP =  Decimal('0.0001')
+STEP = Decimal('0.001')
 MIN_HEIGHT = 0.00001
-MAX_T = 10
+MAX_T = 100
 
 ba = 0.1
 m = 1
 b = 30
 g = 9.8
-k = 100000
+k = 10000
 
 
+def calculate_next_height(previous_height, previous_velocity):
+    return float(STEP) * previous_velocity + previous_height
 
 
-def calculate_next_height(previously_height, previously_velocity):
-    return float(STEP) * previously_velocity + previously_height
-    
-
-def calculate_next_velocity(previously_velocity, previously_aceleration):
-    return float(STEP) * previously_aceleration + previously_velocity
+def calculate_next_velocity(previous_velocity, previous_acceleration):
+    return float(STEP) * previous_acceleration + previous_velocity
 
 
-def calculate_aceleration(previously_height, previously_velocity):
-    if(previously_height > 0):        
-        return -ba / m * previously_velocity - g
+def calculate_acceleration(previous_height, previous_velocity):
+    if previous_height > 0:
+        return -ba / m * previous_velocity - g
     else:
-        return -k * previously_height - ba * previously_velocity - g
+        return -k * previous_height - ba * previous_velocity - g
+
+
+def check_change(current_height, previous_height, ascending):
+    if ascending and current_height < previous_height:
+        return True
+    if not ascending and previous_height < current_height:
+        return True
+    return False
 
 
 def generateData():
     t = Decimal('0')
-
-    previously_velocity = 0.0
-    previously_height = 10
-    previously_aceleration = calculate_aceleration(previously_height, previously_velocity)
-    data = {'time': float(t), 'height': previously_height, 'velocity': previously_velocity, 'aceleration': previously_aceleration}
+    previous_velocity = 0.0
+    previous_height = 10
+    previous_acceleration = calculate_acceleration(previous_height, previous_velocity)
+    ascending = False
+    data = {'time': float(t), 'height': previous_height, 'velocity': previous_velocity,
+            'acceleration': previous_acceleration}
     df = pd.DataFrame(data, index=[0])
     df.to_csv('data.csv', index=False)
     while t < MAX_T:
-        previously_height =  calculate_next_height(previously_height, previously_velocity)
-        previously_velocity = calculate_next_velocity(previously_velocity, previously_aceleration)
-        previously_aceleration = calculate_aceleration(previously_height, previously_velocity)
-        data = {'time': float(t), 'height': previously_height, 'velocity': previously_velocity, 'aceleration': previously_aceleration}
+        current_height = previous_height  # Implementing
+        previous_height = calculate_next_height(previous_height, previous_velocity)
+        previous_velocity = calculate_next_velocity(previous_velocity, previous_acceleration)
+        previous_acceleration = calculate_acceleration(previous_height, previous_velocity)
+
+        data = {'time': float(t), 'height': previous_height, 'velocity': previous_velocity,
+                'acceleration': previous_acceleration}
         df = pd.DataFrame(data, index=[0])
         df.to_csv('data.csv', mode='a', header=False, index=False)
         t += STEP
-
+        if check_change(current_height, previous_height, ascending):
+            if ascending and MIN_HEIGHT > current_height > 0:
+                t = MAX_T
+            ascending = not ascending
 
 
 def plot_data():
@@ -70,22 +82,14 @@ def plot_data():
     plt.ylabel('Velocidad')
     plt.title('Gráfico de velocidades en función del tiempo')
     plt.grid(True)
-    plt.show()    
+    plt.show()
 
 
 def main():
-    # generateData()
+    generateData()
     plot_data()
+
 
 if __name__ == "__main__":
     main()
     # generateData()
-
-def check_heights_min(t, height):
-    if(heights.__contains__(t-STEP*Decimal(2)) and heights.__contains__(t-STEP)):
-        if heights[t-STEP*Decimal(2)] < heights[t-STEP] and height < heights[t-STEP]:
-            if heights[t-STEP] <= MIN_HEIGHT:
-                return True
-    return False
-
-
